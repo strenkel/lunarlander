1765b563777147f2a055ca9dfb6bb41e74dc9c2a
Stefan Trenkel <info@stefantrenkel.de>
2015-03-08 00:06:35 +0100

    add keyboard (wip)

--- index.php
+++ index.php
@@ -20,6 +20,23 @@
       <input id="fuel-input-template" class="input" type="text" pattern="[0-9]*"/>
     </div>
 
+    <div class="keyboard">
+      <div>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+        <span>5</span>
+      </div>
+      <div>
+        <span>6</span>
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+        <span>0</span>
+      </div>
+    </div>
+
   </body>
   
   <script src="js/LanderMath.js" type="text/javascript"></script>
--- php/menu.php
+++ php/menu.php
@@ -1,13 +1,13 @@
 <nav class="menu">
   <div>
-  <span>
-    <a href="index.php">Start</a>
-  </span>
-  <span>
-    <a href="help.php">Help</a>
-  </span>
-  <span>
-    <a href="contact.php">Contact</a>
-  </span>
+    <span>
+      <a href="index.php">Start</a>
+    </span>
+    <span>
+      <a href="help.php">Help</a>
+    </span>
+    <span>
+      <a href="contact.php">Contact</a>
+    </span>
   </div>
 </nav>
