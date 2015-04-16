bd6fefa2fdb27ffab8433a41e9f7f81b10049139
Stefan Trenkel <info@stefantrenkel.de>
2015-04-16 22:42:23 +0200

    remove php

--- php/head.php
+++ php/head.php
@@ -2,8 +2,8 @@
   <meta HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=utf8">
   <meta name="viewport" content="width=device-width">
   <title>Lunar Lander</title>
-  <meta name="description" content="Remake of the Hewlett Packard 67 lunar landing game.">
-  <meta name="keywords" content="Moon Lander, Lunar Lander, Lunar Landing Game, hp 65, hp 67">
+  <meta name="description" content="Remake of the textbased lunar landing game.">
+  <meta name="keywords" content="Moon Lander, Lunar Lander, Lunar Landing Game, Jim Storer, Computer Game, hp 65, hp 67">
   <meta name="author" content="Stefan Trenkel">
   <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
